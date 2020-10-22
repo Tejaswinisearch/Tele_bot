186f6a73a3c65328e79ae288a2bfe26aa812fb14
Tejaswinisearch <72953157+Tejaswinisearch@users.noreply.github.com>
2020-10-22 11:27:15 +0530

    Update Tejaswini123.py

--- Tejaswini123.py
+++ Tejaswini123.py
@@ -38,6 +38,7 @@ ADAFRUIT_IO_USERNAME = os.getenv('ADAFRUIT_IO_USERNAME')
 ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY')
 TOKEN = os.getenv('TOKEN')
 aio = Client('ADAFRUIT_IO_USERNAME','ADAFRUIT_IO_KEY')
+
 updater=Updater('TOKEN', use_context=True)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('turnoff',turnoff)) 
